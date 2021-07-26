99db87ea49253452ded68ea96c23ecd77e965e47
Kyle Davis <kyledavis@Kyles-iMac.local>
2021-07-26 15:40:10 -0600

    re-seed database

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,5 +2,5 @@ class Employee < ApplicationRecord
   belongs_to :company
 
   validates :name, length: { minimum: 5 }
-  validates :floor, numericality: { only_integer: true }
+  # validates :floor, numericality: { only_integer: true }
 end
